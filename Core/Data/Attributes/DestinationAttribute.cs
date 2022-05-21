ff9c7c7b3c3db052ceaefb61ad8fda7abf0692d7
Damien Delmarle <delmarle.damien@gmail.com>
2022-05-21 13:50:15 +0300

    initial commit

---
+++ Core/Data/Attributes/DestinationAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Station
+{
+    [AttributeUsage(AttributeTargets.Field), Serializable]
+    public class DestinationAttribute : PropertyAttribute
+    {
+        [FormerlySerializedAs("scene")] [FormerlySerializedAs("Destination")] public DestinationModel destination;
+    }
+}
---
+++ Core/Files & Data/DataAccess/IDataContainer.cs
@@ -0,0 +1,10 @@
+﻿namespace Station
+{
+    public interface IDataContainer
+    {
+        void SetPath(string path);
+        T Load<T>();
+        void Save<T>(T data);
+        void Delete();
+    }
+}
